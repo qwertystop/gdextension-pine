264da908c3240035010a61c6d2c879c1482af73a
Vivian Heyman <qwertystop@users.noreply.github.com>
2024-05-04 16:42:22 -0400

    Add register_types

---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef GDPINE_REGISTER_TYPES_H
+#define GDPINE_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_pine_module(ModuleInitializationLevel p_level);
+void uninitialize_pine_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
